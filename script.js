ff6462a54eda1add9015055a41bd3597ce3a646f
Phil <philrsheridan@gmail.com>
2023-09-10 17:03:11 -0400

    added showInfo function, added bio, changed formatting

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-
-document.querySelector("body").appendChild(h2);
+function showInfo(){
+    console.log("click");
+    let bio = document.getElementById("bio");
+    bio.style.display = "block";
+}
