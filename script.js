341af4e1e7357144dcbd744b0927759302a9b51f
Phil <philrsheridan@gmail.com>
2023-09-10 18:16:55 -0400

    added image, details section, and formatting

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ function showInfo(){
     let bio = document.getElementById("bio");
     bio.style.display = "block";
 }
+
+function showImg(){
+    let img = document.getElementById("image");
+    img.style.display = "block";
+}
